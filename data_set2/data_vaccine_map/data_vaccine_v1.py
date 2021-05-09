b92e9bdc85cf876c7b5749693e9f317f9de33859
Mihai <mihai97ionita@gmail.com>
2021-05-09 21:37:30 +0300

    merged tests and vaccine datasets

--- data_set2/data_vaccine_map/data_vaccine_v1.py
+++ data_set2/data_vaccine_map/data_vaccine_v1.py
@@ -8,6 +8,7 @@ input: DataFrame = pandas.read_csv('..///data_vaccine.csv')
 
 # only ALL TargetGroup
 filtered_temp = input[input["TargetGroup"] == "ALL"]
+# only Region as Country
 filtered = filtered_temp[filtered_temp["Region"] == filtered_temp["ReportingCountry"]]
 
 # todo do we want so sum all regions?.. if so remove 'Region' from below
@@ -26,6 +27,35 @@ values: DataFrame = grouped_by_time_location[columns_to_keep].max()
 summed_all_vac_types["TotalDoses"] = summed_all_vac_types[["FirstDose", "SecondDose", "UnknownDose"]].sum(axis=1)
 
 # merge sum operations with values using keys_columns as keys to merge by
-output = pandas.merge(left=summed_all_vac_types, right=values, on=keys_columns)
+merged = pandas.merge(left=summed_all_vac_types, right=values, on=keys_columns)
 
-output.to_csv('dataset2.csv')
+# write tmp dataset for some reason
+dataset_name = 'dataset_vaccine_and_testing.csv'
+merged.to_csv(dataset_name)
+
+# index by YearWeekISO
+merged: DataFrame = pandas.read_csv(dataset_name)
+
+# only keep the essential columns
+essential = merged[["YearWeekISO","ReportingCountry", "TotalDoses"]].copy()
+
+# having YearWeekISO as index (ordered)... cumulative sum for each ReportingCountry
+essential["TotalDosesSum"] = essential.groupby(['ReportingCountry'])["TotalDoses"].cumsum()
+
+# read data regrading tests
+data_testing = pandas.read_csv('..///data_testing.csv')
+
+# keep essential
+testing_essentials = data_testing[["year_week", "country_code", "positivity_rate"]].copy()
+
+# merge vaccine and tests
+final_merge = pandas.merge(left=essential, right=testing_essentials, left_on=['YearWeekISO','ReportingCountry'], right_on=['year_week','country_code'])
+
+# remove duplicates columns
+final_merge = final_merge[["YearWeekISO","ReportingCountry", "TotalDosesSum", "positivity_rate"]].copy()
+
+# set index to date
+final_merge.set_index('YearWeekISO', inplace=True)
+
+# you can read it yourself :)
+final_merge.to_csv(dataset_name)
